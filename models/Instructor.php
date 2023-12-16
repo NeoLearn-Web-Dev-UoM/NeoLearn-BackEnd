0f0198ff3f4cf11c558093bc1bd62d7116906158
ArchontisKostis <arxontisk02@gmail.com>
2023-12-16 18:55:34 +0200

    remove unneeded parameter from Instructor constructor

--- models/Instructor.php
+++ models/Instructor.php
@@ -3,7 +3,7 @@
 class Instructor extends User implements jsonSerializable
 {
 
-    public function __construct($email, $password, $course)
+    public function __construct($email, $password)
     {
         parent::__construct($email, $password);
     }
