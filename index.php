fab675dae60d9da1ef18e7e8ca2d816f9b7972bc
ArchontisKostis <arxontisk02@gmail.com>
2023-12-28 20:47:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-require_once 'config/DatabaseConfig.php';
-require_once 'config/RoutesConfig.php';
-require __DIR__. '/controllers/StudentController.php';
-require __DIR__. '/controllers/AuthenticationController.php';
-require __DIR__. '/controllers/InstructorController.php';
+require_once 'config/Router.php';
 
 /* This is the entry point of the application */
 
@@ -11,9 +7,6 @@ require __DIR__. '/controllers/InstructorController.php';
 $requestUrl = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-// Connect to the database
-$conn = \config\DatabaseConfig::configDatabase();
-
 // Create a new router instance
-$router = new \config\Router($conn);
+$router = new \config\Router();
 $router->handleRequest($requestUrl, $requestMethod);
