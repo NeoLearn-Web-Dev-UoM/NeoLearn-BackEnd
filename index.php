15c32e5d880e66a865c8d83b505819f0e9b7b4a2
ArchontisKostis <arxontisk02@gmail.com>
2024-01-08 14:40:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,11 @@ require_once 'config/RouterConfig.php';
 
 /* This is the entry point of the application */
 
+// Allow cross origin requests
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
 // Parse request url and method
 $requestUrl = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
