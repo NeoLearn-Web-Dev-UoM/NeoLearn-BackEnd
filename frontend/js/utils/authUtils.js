5d61b73e3b594cd682d5e4a318a7dd02f008ce1e
ArchontisKostis <arxontisk02@gmail.com>
2024-01-08 14:39:21 +0200

    Create authUtils.js

---
+++ frontend/js/utils/authUtils.js
@@ -0,0 +1,9 @@
+function logout() {
+    let logoutMsg = "Είσαι σίγουρος ότι επιθυμείς να αποσυνδεθείς από το Λογαριασμό σου;";
+    let logoutConfirmed = confirm(logoutMsg);
+
+    if (logoutConfirmed) {
+        localStorage.removeItem('user');
+        window.location.href = 'welcome.html';
+    }
+}
