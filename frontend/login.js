e39daa16ad2a59b8487e8f0e419853547dc50525
LAPTOP-NM3OL5T8\stavr <stavrosplatisofficial@gmail.com>
2024-01-08 13:29:39 +0200

    login/student

--- frontend/login.js
+++ frontend/login.js
@@ -1,94 +1,129 @@
-'use strict'
+"use strict";
 
 async function loginInstructor(username, password) {
-    let loginAPI = 'http://localhost/neolearn-backend/index.php/auth/instructor/login';
+  let loginAPI =
+    "http://localhost/neolearn-backend/index.php/auth/instructor/login";
 
-    try {
-        return fetch(loginAPI, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "email": username,
-                "password": password,
-            }),
-            mode: 'cors'
-        });
-    }
-    catch (error) {
-        console.error('An error occurred during the fetch:', error);
-    }
+  try {
+    return fetch(loginAPI, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password,
+      }),
+      mode: "cors",
+    });
+  } catch (error) {
+    console.error("An error occurred during the fetch:", error);
+  }
+}
+
+async function loginStudent(username, password) {
+  let loginAPI =
+    "http://localhost/neolearn-backend/index.php/auth/student/login";
+
+  try {
+    return fetch(loginAPI, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password,
+      }),
+      mode: "cors",
+    });
+  } catch (error) {
+    console.error("An error occurred during the fetch:", error);
+  }
 }
 
 async function submitBtn() {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  let userType = getCheckboxValue(checkboxes);
 
-    let userType = getCheckboxValue(checkboxes);
+  if (userType === null) {
+    alert("Please select a user type");
+    return;
+  }
 
-    if (userType === null) {
-        alert('Please select a user type');
-        return;
-    }
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+  if (username === "" || password === "") {
+    alert("Please fill in all fields");
+    return;
+  }
 
-    if (username === '' || password === '') {
-        alert('Please fill in all fields');
-        return;
-    }
+  console.log(userType);
 
-    console.log(userType);
+  if (userType === "teacher") {
+    const response = await loginInstructor(username, password);
+    handleLoginResponse(response);
+  } else if (userType === "student") {
+    const response = await loginStudent(username, password);
+    handleLoginResponse(response);
+  }
+}
 
-    if (userType === 'teacher') {
-        const response = await loginInstructor(username, password);
+function handleLoginResponse(response) {
+  // Get the response body
+  response
+    .json()
+    .then((data) => {
+      let alertBox = document.getElementById("alert-login");
 
-        // Get the response body
-        const data = await response.json();
+      if (response.ok) {
+        alertBox.classList.add("alert-success");
+        alertBox.innerHTML = "Login Successful";
+        alertBox.style.display = "block";
 
-        if (response.ok) {
-            let alertBox = document.getElementById('alert-login');
-            alertBox.classList.add('alert-success');
-            alertBox.innerHTML = 'Επιτυχής σύνδεση';
-            alertBox.style.display = 'block';
+        // Add user to local storage
+        localStorage.setItem("user", JSON.stringify(data));
 
-            // Add user to local storage
-            localStorage.setItem('user', JSON.stringify(data));
-
-            // Redirect to instructor dashboard
-            window.location.href = 'http://localhost/NeoLearn-BackEnd/frontend/teacher.html';
+        // Redirect to dashboard based on user type
+        if (data.user_type === "instructor") {
+          window.location.href =
+            "http://localhost/NeoLearn-BackEnd/frontend/teacher.html";
+        } else if (data.user_type === "student") {
+          window.location.href =
+            "http://localhost/NeoLearn-BackEnd/frontend/student.html";
         }
-        else if (response.status === 401) {
-            let alertBox = document.getElementById('alert-login');
-            alertBox.classList.add('alert-danger');
-            alertBox.innerHTML = 'Λάθος κωδικός ή email';
-            alertBox.style.display = 'block';
+      } else if (response.status === 401) {
+        alertBox.classList.add("alert-danger");
+        alertBox.innerHTML = "Incorrect username or password";
+        alertBox.style.display = "block";
 
-            console.log(response.body);
-        }
-    }
-
+        console.log(response.body);
+      }
+    })
+    .catch((error) => {
+      console.error("Error parsing response body:", error);
+    });
 }
 
 function getCheckboxValue(checkboxes) {
-    let userType = null;
+  let userType = null;
 
-    checkboxes.forEach(function (checkbox) {
-        if (checkbox.checked) {
-            userType = checkbox.id;
-        }
-    });
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      userType = checkbox.id;
+    }
+  });
 
-    return userType;
+  return userType;
 }
 
 function handleCheckboxClick(clickedCheckboxId) {
-    const checkboxes = document.querySelectorAll('.form-check-input');
+  const checkboxes = document.querySelectorAll(".form-check-input");
 
-    checkboxes.forEach(function (checkbox) {
-        if (checkbox.id !== clickedCheckboxId) {
-            checkbox.checked = false;
-        }
-    });
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.id !== clickedCheckboxId) {
+      checkbox.checked = false;
+    }
+  });
 }
--- frontend/student.js
+++ frontend/student.js
@@ -1,49 +1,149 @@
-'use strict'
+"use strict";
 let currentSlide = 1;
-function showSlide(slideNumber) {
-    const slides = document.querySelectorAll('.slide');
-    slides.forEach(slide => {
-        slide.style.display = 'none';
-    });
 
-    const currentSlideElement = document.getElementById(`slide${slideNumber}`);
-    if (currentSlideElement) {
-        currentSlideElement.style.display = 'block';
-        currentSlide = slideNumber;
+// Get the user from local storage
+let user = localStorage.getItem("user");
+
+// ------------ DISPLAY USER NAME ------------
+
+// If the user is not logged in, redirect to the login page
+if (!user) {
+  window.location.href = "welcome.html";
+}
+
+// Turn the user to an object we can work with
+user = JSON.parse(user);
+
+// Get the user's name
+const name = user.name;
+
+// Display the user's name
+const nameElement = document.getElementById("user-name");
+nameElement.innerHTML = name;
+
+// ------------ DISPLAY COURSES ------------
+document.addEventListener("DOMContentLoaded", function () {
+  // Fetch courses from the API
+  fetch(`/neolearn-backend/index.php/students/search/${user.id}/courses`)
+    .then((response) => response.json())
+    .then((courses) => {
+      createCoursesElements(courses);
+    })
+    .catch((error) => console.error("Error fetching courses:", error));
+});
+
+// SEARCH COURSES
+
+// Get the search button
+const searchBtn = document.getElementById("search-btn");
+
+// Add an event listener to the search button
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchCourses();
+});
+
+function searchCourses() {
+  // Fetch courses from the API
+  fetch("http://localhost/neolearn-backend/index.php/courses/search/name", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: document.getElementById("search-course").value,
+    }),
+  })
+    .then((response) => console.log(response.json()))
+    .then((courses) => {
+      console.log(courses);
+      createCoursesElements(courses);
+    })
+    .catch((error) => console.error("Error fetching courses:", error));
+}
+
+function createCoursesElements(courses) {
+  // Handle the data and generate HTML for each course
+  const coursesContainer1 = document.getElementById("courses-container-1");
+  const coursesContainer2 = document.getElementById("courses-container-2");
+
+  courses.forEach((course, index) => {
+    const courseElement = document.createElement("div");
+    courseElement.innerHTML = `
+                    <a class="lesson-link" onclick="openNewPage('lessonText${
+                      index + 1
+                    }', 'sourceTitle${index + 1}')">
+                        <figure>
+                            <blockquote class="blockquote">
+                                <p id="lessonText${index + 1}">${
+      course.name
+    }</p>
+                            </blockquote>
+                            <figcaption class="blockquote-footer">
+                                <cite title="Source Title" id="sourceTitle${
+                                  index + 1
+                                }">${course.description}</cite>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <hr>
+                `;
+
+    // Add the course to the appropriate container based on the slide
+    if (index % 2 === 0) {
+      coursesContainer1.appendChild(courseElement);
+    } else {
+      coursesContainer2.appendChild(courseElement);
     }
+  });
+}
+
+function showSlide(slideNumber) {
+  const slides = document.querySelectorAll(".slide");
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+
+  const currentSlideElement = document.getElementById(`slide${slideNumber}`);
+  if (currentSlideElement) {
+    currentSlideElement.style.display = "block";
+    currentSlide = slideNumber;
+  }
 }
 function changeSlide(nextSlide) {
-    showSlide(nextSlide);
+  showSlide(nextSlide);
 }
 // Show the initial slide
 showSlide(currentSlide);
 
-
 function openNewPage(lessonTextId, sourceTitleId) {
-    var lessonText = document.getElementById(lessonTextId).textContent;
-    var sourceTitle = document.getElementById(sourceTitleId).textContent;
+  var lessonText = document.getElementById(lessonTextId).textContent;
+  var sourceTitle = document.getElementById(sourceTitleId).textContent;
 
-    // Use unique identifiers for each link
-    var keyPrefix = 'lessonDescription_';  // Unique prefix
-    localStorage.setItem(keyPrefix + 'lessonText', lessonText);
-    localStorage.setItem(keyPrefix + 'sourceTitle', sourceTitle);
+  // Use unique identifiers for each link
+  var keyPrefix = "lessonDescription_"; // Unique prefix
+  localStorage.setItem(keyPrefix + "lessonText", lessonText);
+  localStorage.setItem(keyPrefix + "sourceTitle", sourceTitle);
 
-    // Navigate to the new_page.html
-    window.location.href = 'lessonDescription.html';
+  // Navigate to the new_page.html
+  window.location.href = "lessonDescription.html";
 }
 
 function homeBtn() {
-    window.location.href = 'student.html';
+  window.location.href = "student.html";
 }
 
 function newLesson() {
-    window.location.href = 'newLesson.html';
+  window.location.href = "newLesson.html";
 }
 
 function logOutBtn() {
-    var txt;
-    if (confirm("Είσαι σίγουρος ότι επιθυμείς να αποσυνδεθείς από το Λογαριασμό σου;")) {
-        window.location.href = 'welcome.html';
-    }
-    document.getElementById("demo").innerHTML = txt;
+  var txt;
+  if (
+    confirm(
+      "Είσαι σίγουρος ότι επιθυμείς να αποσυνδεθείς από το Λογαριασμό σου;"
+    )
+  ) {
+    window.location.href = "welcome.html";
+  }
+  document.getElementById("demo").innerHTML = txt;
 }
