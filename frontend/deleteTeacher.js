8c50f6697aa63091a79c36b24765111781086852
ArchontisKostis <arxontisk02@gmail.com>
2024-01-09 17:04:43 +0200

    Update deleteTeacher.js

--- frontend/deleteTeacher.js
+++ frontend/deleteTeacher.js
@@ -1,11 +1,5 @@
 'use strict'
 
-//οταν παταει απεγγραφη στο παραθυρο με τη περιγραφη του μαθηματος
-function deleteLessonLink(event) {
-    event.preventDefault();
-    window.location.href = 'deleteLessonTeacher.html';
-}
-
 function showAlert() {
     alert("Το μάθημα διαγράφτηκε με επιτυχία!");
     //εδω θυελω οταν παταω οκ στο αλερτ να νμε οηγαινει στην αρχικη σελιδα με τα μαθηματα χωωρις το μαθηα 
