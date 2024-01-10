3f680e7e1d5e61ba9f9c54e08d10dadb5c8b8171
ArchontisKostis <arxontisk02@gmail.com>
2024-01-10 16:22:01 +0200

    Update utils

--- frontend/js/utils/utils.js
+++ frontend/js/utils/utils.js
@@ -1,3 +1,26 @@
-function setUserNameOnTopDropdown() {
+function updateUserProfileDetailsElement(name, email, userType) {
+    const userProfileDetails = document.getElementById('profile-details');
+    let newElement = document.createElement('p');
+    newElement.innerHTML = `
+        <b> Όνομα: </b> ${name} <br>
+        <b> Email: </b> ${email} <br>
+        <b> Τύπος: </b> ${userType} <br>
+    `;
 
+    userProfileDetails.appendChild(newElement);
 }
+
+function logout() {
+    let logoutMsg = "Είσαι σίγουρος ότι επιθυμείς να αποσυνδεθείς από το Λογαριασμό σου;";
+    let logoutConfirmed = confirm(logoutMsg);
+
+    if (logoutConfirmed) {
+        localStorage.removeItem('user');
+        localStorage.removeItem('userType');
+
+        window.location.href = 'welcome.html';
+    }
+}
+
+
+export { updateUserProfileDetailsElement, logout };
